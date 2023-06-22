12e2c65212eb047c2ed102f4fe8055bd1b77322d
MD-ANIKS <freelanceranik1879@gmail.com>
2023-06-23 00:59:12 +0600

    Updated JavaScript module to accept and sort JSON feed of data

---
+++ public/script.js
@@ -0,0 +1,401 @@
+const DATA = [
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "DEMO 1",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "DEMO 2",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Z ",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Y",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "X",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "X Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "X State",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Z State",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 1,
+    branch_name: "Test Branch",
+    branch_state: "Colorado",
+    branch_city: "Denver",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 2,
+    branch_name: "Another Branch",
+    branch_state: "Demo Colorado",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 3,
+    branch_name: "Another Branch",
+    branch_state: "Florida",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 3,
+    branch_name: "Another Branch",
+    branch_state: "Florida",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 3,
+    branch_name: "Another Branch",
+    branch_state: "Florida",
+    branch_city: "Aspen",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 4,
+    branch_name: "Another Branch",
+    branch_state: "Arizona",
+    branch_city: "Phoenix",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 4,
+    branch_name: "Another Branch",
+    branch_state: "Arizona",
+    branch_city: "Phoenix",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 4,
+    branch_name: "Another Branch",
+    branch_state: "Arizona",
+    branch_city: "Phoenix",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 4,
+    branch_name: "Another Branch",
+    branch_state: "Arizona",
+    branch_city: "Phoenix",
+    branch_link: "http://www.testing.com",
+  },
+  {
+    branch_id: 4,
+    branch_name: "Another Branch",
+    branch_state: "Arizona",
+    branch_city: "Phoenix",
+    branch_link: "http://www.testing.com",
+  },
+];
+
+/**
+ * Sort Data alphabetically by state name
+ */
+DATA.sort((a, b) => a.branch_state.localeCompare(b.branch_state));
+
+const seeAllButton = document.querySelector(".see-all-branch-button");
+const branchesContainer = document.querySelector(".branch-container");
+
+function renderBranch(item) {
+  const branchStateClassName = item.branch_state
+    .toLowerCase()
+    .split(" ")
+    .join("-");
+
+  const elementContainer = document.createElement("div");
+  elementContainer.className =
+    " first:mt-0 element_container " + branchStateClassName;
+
+  const stateContainer = document.createElement("div");
+  stateContainer.className =
+    "branch_state text-lg uppercase font-visby-bold mb-3 mt-12";
+  stateContainer.textContent = item.branch_state;
+
+  const elementItemsContainer = document.createElement("div");
+  elementItemsContainer.className = branchStateClassName;
+
+  const branchContainer = document.createElement("div");
+  branchContainer.className = "text-md";
+
+  const branchNameContainer = document.createElement("a");
+  branchNameContainer.className = "font-visby-extra-bold text-portfolio";
+  branchNameContainer.href = item.branch_link;
+  branchNameContainer.textContent = item.branch_name;
+
+  const branchCityContainer = document.createElement("p");
+  branchCityContainer.className = "text-sm mb-3";
+  branchCityContainer.textContent = item.branch_city;
+
+  elementItemsContainer.appendChild(branchNameContainer);
+  elementItemsContainer.appendChild(branchContainer);
+  elementItemsContainer.appendChild(branchCityContainer);
+  elementContainer.appendChild(elementItemsContainer);
+
+  /**
+   * Get previous state container
+   */
+  const prevContainer = document.querySelector(`.${branchStateClassName}`);
+  if (!prevContainer) {
+    /**
+     * StateName
+     */
+    elementContainer.prepend(stateContainer);
+
+    /**
+     * Branch
+     */
+    branchesContainer.appendChild(elementContainer);
+  } else {
+    prevContainer.appendChild(elementContainer);
+  }
+}
+
+function clear_all_branch() {
+  branchesContainer.innerHTML = "";
+}
+
+function list_10_branch() {
+  clear_all_branch();
+  const data = DATA.slice(0, 10);
+  data.map((item) => renderBranch(item));
+  // remove active class
+  seeAllButton.classList.remove("activated");
+  seeAllButton.innerHTML = "+ See All";
+
+  document.querySelector(".branch_state").classList.remove("mt-8");
+}
+
+function list_all_branch() {
+  clear_all_branch();
+  DATA.map((item) => renderBranch(item));
+  // add active class to indicate already showing all branches
+  seeAllButton.classList.add("activated");
+  seeAllButton.innerHTML = "- See All";
+
+  document.querySelector(".branch_state").classList.add("mt-8");
+}
+
+/**
+ * Don't show see all button
+ * if data is less than or equal to 10
+ */
+if (DATA.length <= 10) {
+  seeAllButton.classList.add("hidden");
+}
+
+seeAllButton.addEventListener("click", () => {
+  if (seeAllButton.classList.contains("activated")) {
+    list_10_branch();
+  } else {
+    list_all_branch();
+  }
+});
+
+list_10_branch();
